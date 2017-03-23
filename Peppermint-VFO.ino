5dd515313f74e824534830fa9ec393c6ea36f996
Bruce MacKinnon <bruce@mackinnon.com>
2017-03-22 21:36:16 -0400

    Working on CAT commands

--- Peppermint-VFO.ino
+++ Peppermint-VFO.ino
@@ -1,47 +1,42 @@
 // BITX40 VFO
 // Bruce MacKinnon KC1FSZ 22-March-2017
 //
-//#include "si5351.h"
+#include "si5351.h"
 #include "Wire.h"
 #include <SPI.h>
 #include <Adafruit_SSD1306.h>
+
 #include <ClickEncoder.h>
 #include <TimerOne.h>
 
-#define OLED_RESET 4
-
-Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_SSD1306 display(4);
 
 Si5351 si5351;
 
-// ------ Encoder Related ----------------------------------------------------------------
-
 ClickEncoder encoder(3,2,4,4);
-
-TimerOne serviceTimer;
 
 void serviceCb() {
   encoder.service();
 }
 
 // The frequency that is displayed 
-long long displayFreq = 720000000LL;
+unsigned long displayFreq = 7200000L;
 // The IF frequency 
-long long ifFreq = 1200000000LL;
+unsigned long ifFreq = 12000000L;
 // The adjustment
-long long adjFreq = 160000LL;
+unsigned long adjFreq = 1600L;
 
 // ----- FREQUENCY STEP MENU ---------------------------------------------------------------
 const unsigned int freqStepMenuSize = 7;
 
 const long long freqStepMenu[freqStepMenuSize] = {
-  100LL,
-  1000LL,
-  10000LL,
-  50000LL,
-  100000LL,
-  1000000LL,
-  10000000LL
+  1L,
+  10L,
+  100L,
+  500L,
+  1000L,
+  10000L,
+  100000L
 };
 
 const char* freqStepMenuText[freqStepMenuSize] = {
@@ -88,19 +83,21 @@ void setup() {
   Timer1.attachInterrupt(serviceCb); 
 
   setFreq(displayFreq);
+
+  pinMode(13,OUTPUT);
 }
 
-void updateDisplayFreq(unsigned long long freq) {
-  unsigned long long f = freq / 100;
+void updateDisplayFreq(unsigned long freq) {
+  unsigned long f = freq;
   unsigned long f0 = f / 1000000;
   unsigned long f1 = (f - (f0 * 1000000)) / 1000;
   unsigned long f2 = f - f0 * 1000000 - f1 * 1000;
   display.setCursor(10,20);
-  display.print((long)f0);
+  display.print(f0);
   display.setCursor(30,20);
-  display.printf("%03d",(long)f1);
+  display.print(f1);
   display.setCursor(70,20);
-  display.printf("%03d",(long)f2);
+  display.print(f2);
 }
 
 void updateDisplay() {
@@ -111,7 +108,7 @@ void updateDisplay() {
   // Top line - logo and mode
   display.setTextSize(1);
   display.setCursor(0,0);
-  display.print("KC1FSZ VFO 2");
+  display.print("KC1FSZ VFO 3");
   display.setCursor(100,0);
   display.print(modeMenuText[mode]);
 
@@ -167,15 +164,47 @@ void updateDisplay() {
   display.display();
 }
 
-void setFreq(long long displayFreq) {
-  unsigned long long a = ifFreq - (displayFreq + adjFreq);
-  si5351.set_freq(a,SI5351_PLL_FIXED, SI5351_CLK0);
-  long f = a / 100ULL;
+void setFreq(unsigned long displayFreq) {
+  unsigned long a = ifFreq - (displayFreq + adjFreq);
+  si5351.set_freq(a * 100,SI5351_PLL_FIXED, SI5351_CLK0);
+}
+
+byte highNibble(byte b) {
+  return (b >> 4) & 0x0f;
+}
+
+byte lowNibble(byte b) {
+  return b & 0x0f;
 }
 
 void processCATCommand(byte* cmd) {
+  // Set frequency
   if (cmd[4] == 1) {
-    
+    // Pull off each of the digits
+    byte d7 = highNibble(cmd[0]);
+    byte d6 = lowNibble(cmd[0]);
+    byte d5 = highNibble(cmd[1]);
+    byte d4 = lowNibble(cmd[1]); 
+    byte d3 = highNibble(cmd[2]);
+    byte d2 = lowNibble(cmd[2]); 
+    byte d1 = highNibble(cmd[3]);
+    byte d0 = lowNibble(cmd[3]); 
+    unsigned long freq = 
+      (unsigned long)d7 * 100000000L +
+      (unsigned long)d6 * 10000000L +
+      (unsigned long)d5 * 1000000L + 
+      (unsigned long)d4 * 100000L + 
+      (unsigned long)d3 * 10000L + 
+      (unsigned long)d2 * 0000L + 
+      (unsigned long)d1 * 100L + 
+      (unsigned long)d0 * 10L; 
+    setFreq(freq);       
+  }
+  else if (cmd[4] == 8) {
+    digitalWrite(13,HIGH);
+  }
+  else if (cmd[4] == 88) {
+    digitalWrite(13,LOW);
   }
 }
 
@@ -193,7 +222,7 @@ void loop() {
     }
     cmdBuf[cmdBufPtr++] = Serial.read();
     // Check to see if we have a full read
-    if (cmdByfPtr == 5) {
+    if (cmdBufPtr == 5) {
       processCATCommand(cmdBuf);
     }
   }
